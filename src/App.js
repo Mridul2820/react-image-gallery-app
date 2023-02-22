440517f3ede4a5f86b657ec29aefcb7eb815553b
Mridul2820 <immridulpanda2820@gmail.com>
2023-02-22 15:03:10 +0530

    Image Gallery Created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ImageGallery from "react-image-gallery";
+import "./App.css";
+import "react-image-gallery/styles/css/image-gallery.css";
 
-function App() {
+const App = () => {
+  const images = [
+    {
+      original:
+        "https://images.pexels.com/photos/831889/pexels-photo-831889.jpeg?auto=compress&cs=tinysrgb&w=1280",
+      thumbnail:
+        "https://images.pexels.com/photos/831889/pexels-photo-831889.jpeg?auto=compress&cs=tinysrgb&w=600",
+      description: "Description 1",
+    },
+    {
+      original:
+        "https://images.pexels.com/photos/1683492/pexels-photo-1683492.jpeg?auto=compress&cs=tinysrgb&w=1280",
+      thumbnail:
+        "https://images.pexels.com/photos/1683492/pexels-photo-1683492.jpeg?auto=compress&cs=tinysrgb&w=600",
+      description: "Description 2",
+    },
+    {
+      original:
+        "https://images.pexels.com/photos/1630128/pexels-photo-1630128.jpeg?auto=compress&cs=tinysrgb&w=1280",
+      thumbnail:
+        "https://images.pexels.com/photos/1630128/pexels-photo-1630128.jpeg?auto=compress&cs=tinysrgb&w=600",
+      description: "Description 3",
+    },
+    {
+      original:
+        "https://images.pexels.com/photos/2273664/pexels-photo-2273664.jpeg?auto=compress&cs=tinysrgb&w=1280",
+      thumbnail:
+        "https://images.pexels.com/photos/2273664/pexels-photo-2273664.jpeg?auto=compress&cs=tinysrgb&w=600",
+      description: "Description 4",
+    },
+    {
+      original:
+        "https://images.pexels.com/photos/572740/pexels-photo-572740.jpeg?auto=compress&cs=tinysrgb&w=1280",
+      thumbnail:
+        "https://images.pexels.com/photos/572740/pexels-photo-572740.jpeg?auto=compress&cs=tinysrgb&w=600",
+      description: "Description 5",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ImageGallery
+      items={images}
+      showThumbnails={false}
+      showFullscreenButton={false}
+      autoPlay={true}
+      slideInterval={5000}
+    />
   );
-}
+};
 
 export default App;
